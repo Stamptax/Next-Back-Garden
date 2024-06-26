d211bbae542fa15be2859939ab50144c90482320
Stamptax <107790849+Stamptax@users.noreply.github.com>
2024-06-26 23:46:47 +0930

    chore: Update DetailedPost component with NavBar and Separator

--- src/app/my-chilling-corner/[postId]/page.js
+++ src/app/my-chilling-corner/[postId]/page.js
@@ -1,4 +1,6 @@
 import { supabase } from "@/lib/client";
+import NavBar from "@/component/NavBar";
+import { Separator } from "@/components/ui/separator";
 export default async function DetailedPost({ params }) {
   const { postId } = params;
   async function fetchThePost(postId) {
@@ -26,11 +28,19 @@ export default async function DetailedPost({ params }) {
     return <div>Post not found or error occurred</div>;
   }
   return (
-    <div>
-      <h1>{thePost.title}</h1>
-      <span>{thePost.author}</span>
-      <span>{thePost.posted_at}</span>
-      <p>{thePost.content}</p>
+    <div className="flex flex-col items-center bg-lime-50 min-h-dvh gap-10">
+      <NavBar />
+      <div className="text-wrap w-2/3 max-w-screen-lg flex flex-col gap-3">
+        <h1 className="text-xl font-extrabold">{thePost.title}</h1>
+        <div className="flex items-end justify-between ">
+          <span className="text-emerald-700">{thePost.author}</span>
+          <span className="text-gray-500">{thePost.posted_at}</span>
+        </div>
+        <Separator className="bg-[#d7e3bc]" />
+      </div>
+      <article className="text-wrap w-2/3 max-w-screen-lg">
+        <p className="text-gray-700 ">{thePost.content}</p>
+      </article>
     </div>
   );
 }
